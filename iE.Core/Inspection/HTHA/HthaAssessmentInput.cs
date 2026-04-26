ec647ddda14fb6089819bfd0e30a4168c45af37b
Chris H. <83517703+ChrisHoover7684@users.noreply.github.com>
2026-04-26 09:58:39 -0500

    Add HTHA assessment logic to iE.Core

---
+++ iE.Core/Inspection/HTHA/HthaAssessmentInput.cs
@@ -0,0 +1,9 @@
+namespace iE.Core.Inspection.HTHA
+{
+    public class HthaAssessmentInput
+    {
+        public string Material { get; set; } = string.Empty;
+        public double TemperatureF { get; set; }
+        public double HydrogenPartialPressurePsia { get; set; }
+    }
+}

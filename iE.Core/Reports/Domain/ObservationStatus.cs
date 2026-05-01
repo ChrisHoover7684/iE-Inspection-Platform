418c059cf5c17190bd3c6524e9c9a9cb7201a420
Chris H. <83517703+ChrisHoover7684@users.noreply.github.com>
2026-05-01 06:53:04 -0500

    Reorganize report domain types into separate files

---
+++ iE.Core/Reports/Domain/ObservationStatus.cs
@@ -0,0 +1,9 @@
+namespace iE.Core.Reports.Domain;
+
+public enum ObservationStatus
+{
+    Acceptable = 1,
+    Finding = 2,
+    NotInspected = 3,
+    NotApplicable = 4
+}
--- iE.Core/Reports/Domain/ReportReviewHistory.cs
+++ iE.Core/Reports/Domain/ReportReviewHistory.cs
@@ -1,12 +1,5 @@
 namespace iE.Core.Reports.Domain;
 
-public enum ReviewAction
-{
-    SubmittedForReview = 1,
-    Approved = 2,
-    ReturnedForRevision = 3
-}
-
 public class ReportReviewHistory
 {
     public string Id { get; set; } = string.Empty;

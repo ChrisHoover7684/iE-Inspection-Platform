2a84228a5ecb56b148b2de9decb656d7642a165e
Chris H. <83517703+ChrisHoover7684@users.noreply.github.com>
2026-05-11 13:40:30 -0500

    Update NdeNumberingTests for 4-digit request/report formats

--- iE.Tests/NdeNumberingTests.cs
+++ iE.Tests/NdeNumberingTests.cs
@@ -3,17 +3,17 @@ using iE.Api.Workflow;
 public class NdeNumberingTests
 {
     [Theory]
-    [InlineData(2026, 1, "NDE-26-001")]
-    [InlineData(2026, 10, "NDE-26-010")]
-    public void FormatRequestNumber_Uses_Standard_Format(int year, int sequence, string expected)
+    [InlineData(2026, 1, "NDE-26-0001")]
+    [InlineData(2026, 10, "NDE-26-0010")]
+    public void FormatRequestNumber_Uses_Minimum_Four_Digit_Format(int year, int sequence, string expected)
     {
         Assert.Equal(expected, NdeNumbering.FormatRequestNumber(year, sequence));
     }
 
     [Theory]
-    [InlineData(2026, "UT Thickness", 1, "RPT-26-UT-001")]
-    [InlineData(2026, "PAUT", 6, "RPT-26-PAUT-006")]
-    public void FormatReportNumber_Uses_Standard_Format(int year, string method, int sequence, string expected)
+    [InlineData(2026, "UT Thickness", 1, "RPT-26-UT-0001")]
+    [InlineData(2026, "PAUT", 6, "RPT-26-PAUT-0006")]
+    public void FormatReportNumber_Uses_Minimum_Four_Digit_Format(int year, string method, int sequence, string expected)
     {
         Assert.Equal(expected, NdeNumbering.FormatReportNumber(year, method, sequence));
     }
